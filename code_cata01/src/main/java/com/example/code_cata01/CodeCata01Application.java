91f0d126a8238e9602f447caf1b8506ce06382ac
Ricardo Manoel <ricardo.manoel@kuehne-nagel.com>
2024-01-24 09:51:48 +0000

    first exercise

---
+++ code_cata01/src/main/java/com/example/code_cata01/CodeCata01Application.java
@@ -0,0 +1,13 @@
+package com.example.code_cata01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCata01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCata01Application.class, args);
+    }
+
+}
---
+++ code_cata01/src/test/java/com/example/code_cata01/CodeCata01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.code_cata01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCata01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
